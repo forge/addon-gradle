5f9c56f3a55cf9ece659a85d7fc166b6ff068525
Adam Wyłuda <adamwyl92@gmail.com>
2013-07-02 22:35:20 +0200

    Added all common gradle plugin types.

--- api/src/main/java/org/jboss/forge/addon/gradle/projects/model/GradlePluginType.java
+++ api/src/main/java/org/jboss/forge/addon/gradle/projects/model/GradlePluginType.java
@@ -16,6 +16,27 @@ import java.util.Map;
  */
 public enum GradlePluginType
 {
+   JAVA("org.gradle.api.plugins.JavaPlugin"), GROOVY("org.gradle.api.plugins.GroovyPlugin"),
+   SCALA("org.gradle.api.plugins.scala.ScalaPlugin"), WAR("org.gradle.api.plugins.WarPlugin"),
+   EAR("org.gradle.plugins.EarPlugin"), JETTY("org.gradle.api.plugins.jetty.JettyPlugin"),
+   CHECKSTYLE("org.gradle.api.plugins.quality.CheckstylePlugin"),
+   CODENARC("org.gradle.api.plugins.quality.CodeNarcPlugin"),
+   FIND_BUGS("org.gradle.api.plugins.quality.FindBugsPlugin"),
+   JDEPEND("org.gradle.api.plugins.quality.JDependPlugin"),
+   PMD("org.gradle.api.plugins.quality.PmdPlugin"), JACOCO("org.gradle.testing.jacoco.plugins.JacocoPlugin"),
+   SONAR("org.gradle.api.plugins.sonar.SonarPlugin"), OSGI("org.gradle.api.plugins.osgi.OsgiPlugin"),
+   ECLIPSE("org.gradle.plugins.ide.eclipse.EclipsePlugin"), IDEA("org.gradle.plugins.ide.idea.IdeaPlugin"),
+   ANTLR("org.gradle.api.plugins.antlr.AntlrPlugin"), PROJECT_REPORTS("org.gradle.api.plugins.ProjectReportsPlugin"),
+   ANNOUNCE("org.gradle.api.plugins.announce.AnnouncePlugin"),
+   BUILD_ANNOUNCEMENTS("org.gradle.api.plugins.announce.BuildAnnouncementsPlugin"),
+   DISTRIBUTION("org.gradle.api.distribution.plugins.DistributionPlugin"),
+   APPLICATION("org.gradle.api.plugins.ApplicationPlugin"),
+   JAVA_LIBRARY_DISTRIBUTION("org.gradle.api.plugins.JavaLibraryDistributionPlugin"),
+   BUILD_DASHBOARD("org.gradle.api.reporting.plugins.BuildDashboardPlugin"),
+   MAVEN("org.gradle.api.plugins.MavenPlugin"), SIGNING("org.gradle.plugins.signing.SigningPlugin"),
+   IVY_PUBLISH("org.gradle.api.publish.ivy.plugins.IvyPublishPlugin"),
+   MAVEN_PUBLISH("org.gradle.api.publish.maven.plugins.MavenPublishPlugin"),
+
    OTHER("");
 
    private static class TypeContainer
@@ -30,15 +51,17 @@ public enum GradlePluginType
       this.clazz = clazz;
       TypeContainer.TYPE_MAP.put(clazz, this);
    }
-   
-   public String getClazz() {
+
+   public String getClazz()
+   {
       return clazz;
    }
-   
+
    /**
     * @return Plugin type for given class. If there is no such type then it returns {@link #OTHER}.
     */
-   public static GradlePluginType typeByClazz(String clazz) {
+   public static GradlePluginType typeByClazz(String clazz)
+   {
       GradlePluginType type = TypeContainer.TYPE_MAP.get(clazz);
       return type != null ? type : OTHER;
    }
